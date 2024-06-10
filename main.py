43d1ff0e295cc706f4626d5699a2273e6e7b3604
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2024-06-10 18:16:43 -0300

    .

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ link = 'http://bianca.com/'
 requisicao = requests.get(link)
 print(requisicao)
 # print(requisicao.text)
-site = BeautifulSoup(requisicao.text,"html.parser")
-# print(site.prettify())
-
-# print(site.title)
-titulo = site.find_all("title")
-corpo = site.find_all("h1")
-print("titulo: ",titulo)
-print("corpo: ",corpo)
+if requisicao.status_code == 200:
+    # Analisa o conteúdo HTML da página
+    site = BeautifulSoup(requisicao.text, "html.parser")
+    
+    titulo = site.find_all("title")
+    corpo = site.find_all("h1")
+    
+    print("titulo: ", titulo)
+    print("corpo: ", corpo)
+else:
+    print("Falha na requisição, status code:", requisicao.status_code)
